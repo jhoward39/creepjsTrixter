c91aa16a2542cbc89e09816b356f6beb57e6f788
Joseph Howard <joeywhoward@gmail.com>
2024-06-30 22:40:22 -0400

    wow that didn't work'

---
+++ trixter.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer-extra')
+
+// add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+
+// puppeteer usage as normal
+puppeteer.launch({ headless: true }).then(async browser => {
+  console.log('Running tests..')
+  const page = await browser.newPage()
+  await page.goto('https://abrahamjuliot.github.io/creepjs/')
+  await sleep(3000)
+  await page.screenshot({ path: 'testresult.png', fullPage: true })
+  await browser.close()
+  console.log(`All done, check the screenshot. ✨`)
+})
